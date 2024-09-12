da05e232d8ec18feb524739b07fe7b7335d67250
jordana983 <jordana.santos@sptech.school>
2024-09-12 19:50:12 -0300

    Tela Inicial - Ajuste

--- App/app/src/main/java/com/example/mobileseniorcare/MainActivity.kt
+++ App/app/src/main/java/com/example/mobileseniorcare/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mobileseniorcare.ui.theme.MobileSeniorCareTheme
@@ -40,19 +37,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
 fun TelaInicial(name: String, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxSize()
-
     ) {
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .background(color = Color(0xFF077DB0))
         ) {
+            Text(
+                text = "Sênior Care",
+                color = Color.White,
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .padding(20.dp)
+            )
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -61,15 +63,8 @@ fun TelaInicial(name: String, modifier: Modifier = Modifier) {
                         shape = androidx.compose.foundation.shape.RoundedCornerShape(topEnd = 30.dp, topStart = 30.dp)
                     )
                     .align(Alignment.BottomCenter) // Alinha a caixa ao fundo
-                    .padding(top = 560.dp) // O padding do topo é necessário para criar o espaço superior
-            ) {
-                Text(
-                    text = "Sênior Care",
-                    modifier = Modifier
-                        .align(Alignment.TopStart)
-                        .padding(20.dp) // Ajustar o padding conforme necessário
-                )
-            }
+                    .padding(top = 620.dp) // O padding do topo é necessário para criar o espaço superior
+            )
         }
     }
 }
@@ -78,6 +73,6 @@ fun TelaInicial(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun TelaInicialPreview() {
     MobileSeniorCareTheme {
-       TelaInicial("Preview")
+        TelaInicial("Preview")
     }
 }
